d5d76ba49f843f95033af6ed73e8e8d6d194e351
lucaisacoder <lucas_hsueh@hotmail.com>
2020-01-28 20:57:38 +0800

    build cmake framework
    Change-Id: Ic298c1277d5791b9ac551ecefc379767de306b53

---
+++ cmake/FindOsal.cmake
@@ -0,0 +1,25 @@
+set(Osal_ROOT_DIRS ${PROJ_REPO_TOP_DIR}/modules/osal)
+
+set(_WINDOWS FALSE)
+if("${CMAKE_SYSTEM_NAME}" MATCHES "Windows")
+    set(_WINDOWS TRUE)
+endif()
+
+set(_LINUX FALSE)
+if("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+    set(_LINUX TRUE)
+endif()
+
+find_path(Osal_INCLUDE_DIR
+    NAMES "osal_api.h"
+    PATHS "${Osal_ROOT_DIRS}/*")
+
+find_library(Osal_LIBRARIES
+    NAMES "libosal.a"
+    HINTS "${Osal_ROOT_DIRS}/output")
+
+if(Osal_INCLUDE_DIR)
+    set(Osal_FOUND true)
+    set(Osal_INCLUDE_DIRS ${Osal_INCLUDE_DIR})
+endif()
+
